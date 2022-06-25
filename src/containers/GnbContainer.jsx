616aea00e0528fdfaaa18bf18cbebd93319e82a1
Eunseo Kim <kim.eunseo@kakao.com>
2022-06-26 01:42:14 +0900

    GNB UI 수정

--- src/containers/GnbContainer.jsx
+++ src/containers/GnbContainer.jsx
@@ -11,12 +11,15 @@ import { AiFillHome, AiOutlineHeart, AiOutlineSend } from 'react-icons/ai';
 import { removeItem  } from '../services/storage';
 
 const Wrapper = styled.div({
+  zIndex: '100',
   width: '100vw',
   padding: '5px',
   display: 'flex',
   position: 'fixed',
+  backgroundColor: 'white',
   justifyContent: 'space-around',
   borderBottom: '1px solid lightgray',
+  boxShadow: 'rgba(99, 99, 99, 0.1) 0px 2px 8px 0px',
   '@media (max-width: 767px)': {
     justifyContent: 'space-between',
   }
